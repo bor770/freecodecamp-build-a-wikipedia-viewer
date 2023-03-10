46e627a271643bf3c944f49abb5ef7ced6349569
Baruch Giverts <bor770@gmail.com>
2023-03-10 16:35:40 +0200

    added form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +13,7 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   selector: 'app-search',
   standalone: true,
@@ -19,9 +21,19 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './search.component.html',
 })
 export class SearchComponent {
+  form = new FormGroup({ searchTerm: new FormControl() });
   searchBox = false;
 
-  toggleSearchBox() {
-    this.searchBox = !this.searchBox;
+  showSearchBox() {
+    this.searchBox = true;
+  }
+
+  hideSearchBox() {
+    this.searchBox = false;
+    this.form.reset();
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 }
