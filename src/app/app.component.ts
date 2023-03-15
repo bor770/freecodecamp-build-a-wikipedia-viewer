e1bb711d774be5e86d859190b8cf5e7781bcb349
Baruch Giverts <bor770@gmail.com>
2023-03-15 14:54:47 +0200

    new search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { SearchComponent } from './search/search.component';
-
 @Component({
-  imports: [CommonModule, RouterModule, SearchComponent],
+  imports: [CommonModule, RouterModule],
   selector: 'app-root',
   standalone: true,
   styleUrls: ['./app.component.css'],
