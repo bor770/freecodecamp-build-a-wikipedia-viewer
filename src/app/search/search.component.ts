1d5656bfda460e8cae5664ca7f40801a7dec50e7
Baruch Giverts <bor770@gmail.com>
2023-03-15 20:29:37 +0200

    in middle

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,42 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { Router } from '@angular/router';
+
+import { SearchBoxComponent } from '../shared/search-box/search-box.component';
 
 @Component({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, SearchBoxComponent],
   selector: 'app-search',
   standalone: true,
   styleUrls: ['./search.component.css'],
   templateUrl: './search.component.html',
 })
-export class SearchComponent {
-  form = new FormGroup({ searchTerm: new FormControl() });
-  searchBox = false;
-
-  showSearchBox() {
-    this.searchBox = true;
-  }
-
-  hideSearchBox() {
-    this.searchBox = false;
-    this.form.reset();
-  }
-
-  onSubmit() {
-    this.router.navigate([this.form.value.searchTerm]);
-  }
-
-  constructor(private router: Router) {}
-}
+export class SearchComponent {}
--- src/app/shared/search-box/search-box.component.ts
+++ src/app/shared/search-box/search-box.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
   selector: 'app-search-box',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./search-box.component.css'],
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
 })
 export class SearchBoxComponent {
+  @Input() open: boolean;
 
+  onOpen() {
+    this.open = true;
+  }
+
+  onClose() {
+    this.router.navigate([`/d`]);
+  }
+
+  constructor(private router: Router) {}
 }
