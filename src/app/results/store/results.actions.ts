81f24b14c5e655314c21a6870bab6bb26c7cae7c
Baruch Giverts <bor770@gmail.com>
2023-04-09 17:39:11 +0300

    in middle

--- src/app/results/store/results.actions.ts
+++ src/app/results/store/results.actions.ts
@@ -1,13 +1,20 @@
-import { Action } from '@ngrx/store';
+import { Action, createAction, props } from '@ngrx/store';
 
 import { Results } from '../results.model';
 
-export const SET_RESULTS = `[Results] Set Results`;
+// export const SET_RESULTS = `[Results] Set Results`;
 
-export class SetResults implements Action {
-  readonly type = SET_RESULTS;
+// export class SetResults implements Action {
+//   readonly type = SET_RESULTS;
 
-  constructor(public payload: Results) {}
-}
+//   constructor(public payload: Results) {}
+// }
 
-export type ResultsActions = SetResults;
+// export type ResultsActions = SetResults;
+
+export const setResults = createAction(
+  `[Results] Set Results`,
+  props<{ results: Results }>
+);
+
+export type ResultsActions = ReturnType<typeof setResults>;
